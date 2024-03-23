1acebdb68ca55c2964710ce4fcf3935d550aafd0
Selim Hasenekoğlu <101149941+hasenekoglu@users.noreply.github.com>
2024-03-24 01:10:40 +0300

    firstCommit

--- src/demoProjects/rentACar/Domain/Entities/Brand.cs
+++ src/demoProjects/rentACar/Domain/Entities/Brand.cs
@@ -9,6 +9,16 @@ namespace Domain.Entities
 {
     public class Brand : Entity
     {
+        public string Name { get; set; }
 
+        public Brand()
+        {
+        }
+
+        public Brand(int id, string name) : this()
+        {
+            Id = id;
+            Name=name;
+        }
     }
 }
--- src/demoProjects/rentACar/WebAPI/Program.cs
+++ src/demoProjects/rentACar/WebAPI/Program.cs
@@ -8,6 +8,12 @@ namespace WebAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddControllers();
+          //  builder.Services.AddApplicationServices();
+           // builder.Services.AddSecurityServices();
+          builder.Services.AddPersistenceServices(builder.Configuration);
+           // builder.Services.AddInfrastructureServices();
+           // builder.Services.AddHttpContextAccessor();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
