7a9e4d6860702af0982572004187a5c84014165b
Selim Hasenekoğlu <101149941+hasenekoglu@users.noreply.github.com>
2024-04-20 01:38:26 +0300

    Day 3 Add GetByIdQuery

--- src/demoProjects/rentACar/Application/ApplicationServiceRegistration.cs
+++ src/demoProjects/rentACar/Application/ApplicationServiceRegistration.cs
@@ -7,6 +7,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Application.Features.Brands.Rules;
+using Core.Application.Pipelines.Validation;
 using FluentValidation;
 
 namespace Application
@@ -25,7 +26,7 @@ namespace Application
             //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CachingBehavior<,>));
             //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CacheRemovingBehavior<,>));
             //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
-            //services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestValidationBehavior<,>));
 
             return services;
 
