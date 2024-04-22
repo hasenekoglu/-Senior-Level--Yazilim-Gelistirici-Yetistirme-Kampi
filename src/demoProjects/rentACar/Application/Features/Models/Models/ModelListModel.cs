a38887812c482be024639bb8e9d9cf534d2a1e3d
Selim Hasenekoğlu <101149941+hasenekoglu@users.noreply.github.com>
2024-04-22 18:31:36 +0300

    Add Model Feature

---
+++ src/demoProjects/rentACar/Application/Features/Models/Models/ModelListModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Features.Models.Dtos;
+using Core.Persistence.Paging;
+
+namespace Application.Features.Models.Models
+{
+    public class ModelListModel :BasePageableModel
+    {
+
+        public IList<ModelListDto> Items { get; set; }
+
+    }
+}
---
+++ src/demoProjects/rentACar/Application/Features/Models/Queries/GetListModel/GetListModelQuery.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Features.Models.Models;
+using Application.Services.Repositories;
+using AutoMapper;
+using Core.Application.Requests;
+using Core.Persistence.Paging;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.Models.Queries.GetListModel
+{
+    public class GetListModelQuery : IRequest<ModelListModel>
+    {
+        public PageRequest PageRequest { get; set; }
+
+        public class GetListQueryHandler : IRequestHandler<GetListModelQuery, ModelListModel>
+        {
+            private readonly IMapper _mapper;
+            private readonly IModelRepository _modelRepository;
+
+            public GetListQueryHandler(IMapper mapper, IModelRepository modelRepository)
+            {
+                _mapper = mapper;
+                _modelRepository = modelRepository;
+            }
+
+
+            public async Task<ModelListModel> Handle(GetListModelQuery request, CancellationToken cancellationToken)
+            {   
+                            //car models
+               IPaginate<Model> models = await _modelRepository.GetListAsync(include: 
+                                                    m=>m.Include(c=>c.Brand),
+                                                    index: request.PageRequest.Page,
+                                                    size:request.PageRequest.PageSize);
+                            //dataModel
+               ModelListModel mappedModels = _mapper.Map<ModelListModel>(models);
+               return mappedModels;
+            }
+        }
+    }
+}
